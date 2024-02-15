7715855b95babb5480ff4f5a38cb1e45db2b162a
Chhiring Sherpa <158386026+sherpa200586649@users.noreply.github.com>
2024-02-15 08:51:54 -0500

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Confirmation Page</title>
+  <link rel="stylesheet" href="backend.css">
+</head>
+<body>
+  <h1><small>Thank you for your order!</small></h1>
+  <p>We've received your submission.</p> 
+  <p>we will let you know after your order is ready </p>
+  <aside>
+    <img src="images/pizza2.jpg" alt="">
+  </aside>
+</body>
+</html>
